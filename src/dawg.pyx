c4d4054314529f6e47a1c18da3341acc71c0e187
sonakshi <sonakshi1104@gmail.com>
2024-03-07 16:05:24 -0500

    changed results.push_back(string ... to str) in src/dawg.pyx

--- src/dawg.pyx
+++ src/dawg.pyx
@@ -569,7 +569,7 @@ cdef class BytesDAWG(CompletionDAWG):
                 self._completer.length(),
                 b64_decoder_storage
             )
-            results.push_back(string(b64_decoder_storage, key_len))
+            results.push_back(str(b64_decoder_storage, key_len))
 
         return results
 
